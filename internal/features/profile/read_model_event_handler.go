281ac918da9bafe3234c2a4e5330646a8858c80f
Ore <orepeace@gmail.com>
2026-07-21 12:18:38 +0100

    refactor: align profile event query construction

--- internal/features/profile/read_model_event_handler.go
+++ internal/features/profile/read_model_event_handler.go
@@ -82,11 +82,16 @@ func (h *ReadModelEventHandler) handle(ctx context.Context, resolved eventstore.
 }
 
 func readModelEventHandlerQuery() eventstore.Query {
-	return eventstore.Query{Criteria: []eventstore.Criterion{
-		{Tags: []eventstore.Tag{{Key: "eventType", Value: userRegistered}}},
-		{Tags: []eventstore.Tag{{Key: "eventType", Value: userNameChanged}}},
-		{Tags: []eventstore.Tag{{Key: "eventType", Value: ProfileBioUpdated}}},
-		{Tags: []eventstore.Tag{{Key: "eventType", Value: ProfileImageUploaded}}},
-		{Tags: []eventstore.Tag{{Key: "eventType", Value: ProfileHeaderImageUploaded}}},
-	}}
+	eventTypes := []string{
+		userRegistered,
+		userNameChanged,
+		ProfileBioUpdated,
+		ProfileImageUploaded,
+		ProfileHeaderImageUploaded,
+	}
+	criteria := make([]eventstore.Criterion, 0, len(eventTypes))
+	for _, eventType := range eventTypes {
+		criteria = append(criteria, eventstore.Criterion{Tags: []eventstore.Tag{{Key: "eventType", Value: eventType}}})
+	}
+	return eventstore.Query{Criteria: criteria}
 }
